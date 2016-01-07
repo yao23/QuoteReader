4a05c5e7b3bcbe7002050bd5959536caa81c3920
yao <myspiritcrazy@gmail.com>
2016-01-07 10:26:39 -0800

    create the adapter

--- app/src/main/java/com/clouds/liyao/quotereader/ReadQuoteActivity.java
+++ app/src/main/java/com/clouds/liyao/quotereader/ReadQuoteActivity.java
@@ -1,15 +1,66 @@
 package com.clouds.liyao.quotereader;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ReadQuoteActivity extends AppCompatActivity {
+
+    public class QuoteAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private LayoutInflater mInflator;
+        private  DataSource mDataSource;
+
+        public QuoteAdapter(Context c) {
+            mContext = c;
+            mInflator = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mDataSource = new DataSource();
+        }
+
+        @Override
+        public int getCount() {
+            return mDataSource.getDataSourceLength();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView thumbnail;
+            TextView quote;
+
+            if (convertView == null) {
+                convertView = mInflator.inflate(R.layout.list_item_layout, parent, false);
+            }
+
+            thumbnail = (ImageView) convertView.findViewById(R.id.list_thumb);
+            thumbnail.setImageResource(mDataSource.getmPhotoPool().get(position));
+            quote = (TextView) convertView.findViewById(R.id.list_text);
+            quote.setText(mDataSource.getmQuotePool().get(position));
+
+            return convertView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
